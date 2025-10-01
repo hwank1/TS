e31a223e7430d3f9b118b83b47579d77be173192
hwank1 <kijh4383@gmail.com>
2025-10-02 01:04:04 +0900

    TS test

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello TypeScript");
+// const nums: number[] = []
+// const a = 10 + nums[]
+const someString = "typeScript!";
+someString.endsWith("!");
+const util_1 = require("./util");
+(0, util_1.printName)("typeScript");
+let myString = "hi";
+let myNumber = 3;
+let myBoolean = true;
+let myNull = null;
+let myAny = "haha";
+let myAny2 = 1;
+console.log(`myString : ${myString},
+  myNumber : ${myNumber},
+  myBoolean : ${myBoolean},
+  myNull : ${myNull},
+  myAny : ${myAny}
+  myAny : ${myAny2}`);
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+console.log("Hello TypeScript")
+
+// const nums: number[] = []
+
+// const a = 10 + nums[]
+
+const someString = "typeScript!";
+
+someString.endsWith("!");
+
+import{ printName} from "./util"
+
+printName("typeScript")
+
+let myString: string = "hi"
+let myNumber: number = 3
+let myBoolean: boolean = true
+let myNull: null = null
+let myAny : any = "haha"
+let myAny2 : any = 1;
+
+
+console.log(`myString : ${myString},
+  myNumber : ${myNumber},
+  myBoolean : ${myBoolean},
+  myNull : ${myNull},
+  myAny : ${myAny}
+  myAny : ${myAny2}`)
+  
